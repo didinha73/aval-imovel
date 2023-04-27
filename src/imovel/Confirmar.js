09287f07f123347f9f3121b3761f3abbb35da6eb
Adriane <adriprestes73@gmail.com>
2023-04-26 21:16:47 -0300

    delete firebase

---
+++ src/imovel/Confirmar.js
@@ -0,0 +1,31 @@
+import { Button, Dialog, DialogContent, DialogTitle, DialogContentText, DialogActions } from "@mui/material";
+
+export default function Confirmar(props)
+{
+    function cancelar()
+    {
+        props.modificador(false);
+        props.retorno(false);
+    }
+    function confirmar()
+    {
+        props.modificador(false);
+        props.retorno(true);
+    }
+    return (
+        <Dialog open={props.abrir}>
+            <DialogTitle>Confirmar Ação</DialogTitle>
+            <DialogContent>
+                <DialogContentText>
+                    {props.texto}
+                </DialogContentText>
+            </DialogContent>
+
+            <DialogActions>
+                <Button onClick={cancelar}>Cancelar</Button>
+                <Button onClick={confirmar}>Confirmar</Button>
+            </DialogActions>
+        </Dialog>
+    )
+
+}
--- src/imovel/Listar.js
+++ src/imovel/Listar.js
@@ -7,11 +7,17 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import  Button from '@mui/material/Button';
+import { IconButton } from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 import { app } from '../firebase'
 import { getFirestore} from 'firebase/firestore'
 import { collection, getDocs } from 'firebase/firestore';
+import { doc, deleteDoc } from "firebase/firestore";
+
 import { useEffect, useState } from 'react';
+
+import Confirmar from './Confirmar';
 
 //conexao com o banco de dados
 const db = getFirestore(app);
@@ -20,33 +26,60 @@ export default function Listar()
 {
 
     const [imoveis, setImoveis] = useState([]);
+    const [dialogo, setDialogo] = useState(false);
+    const [confirmar, setConfirmar] = useState();
+    const [idSelecionado, setIdSelecionado] = useState();
+
+    async function carregar () 
+    {
+        const resultado = await getDocs(collection(db, "imoveis"));
+        const novo = resultado.docs.map(function(item)
+        {
+            let retorno = item.data();
+            retorno.id = item.id;
+            return retorno;
+        });
+
+        if( imoveis.length == 0 ){
+            setImoveis(novo);
+            console.log(resultado);
+        }    
+        
+        
+    }
 
     useEffect( function(){
 
         let ignore = false;
 
-        async function carregar () 
-        {
-            const resultado = await getDocs(collection(db, "imoveis"));
-            const novo = resultado.docs.map(function(item)
-            {
-                return item.data();
-            });
-
-            if( imoveis.length == 0 ){
-                setImoveis(novo);
-                console.log(resultado);
-            }    
-            
-            
-        }
-     
         carregar();
 
         return () => {
             ignore = true
         }
     }, [imoveis]);
+
+    function deletar(id)
+    {
+        setIdSelecionado(id);
+        setConfirmar(null);
+        setDialogo(true);
+        
+    }
+
+    async function removerFirebase(id)
+    {
+        await deleteDoc(doc(db, "imoveis", id));
+        setImoveis([]);
+        setConfirmar(null);
+        carregar();
+    }
+
+    if(confirmar == true)
+    {
+        removerFirebase(idSelecionado);
+        console.log(idSelecionado);
+    }
 
     return (
         
@@ -63,6 +96,12 @@ export default function Listar()
                     //height: 240,
                   }}
                 >
+                <Confirmar
+                    abrir={dialogo}
+                    texto="Deseja realmente deletar este imóvel?"
+                    modificador={setDialogo}
+                    retorno={setConfirmar}
+                />
                   <Table>
                     <TableHead>
                         <TableRow>
@@ -70,6 +109,7 @@ export default function Listar()
                             <TableCell>Endereço</TableCell>
                             <TableCell>Valor</TableCell>
                             <TableCell>Data de Cadastro</TableCell>
+                            <TableCell>Ações</TableCell>
                         </TableRow>
                     </TableHead>
 
@@ -77,11 +117,14 @@ export default function Listar()
                     {imoveis.map(function (imo) {
                         return (
 
-                            <TableRow key={imo.codigo}>
+                            <TableRow key={imo.id}>
                                 <TableCell>{imo.codigo}</TableCell>
                                 <TableCell>{imo.endereco}</TableCell>
                                 <TableCell>{ imo.valor_avaliado.toLocaleString("pt-BR", {style: "currency", currency: "BRL"}) }</TableCell>
                                 <TableCell>{ imo.data_cadastro.toDate().toLocaleString() }</TableCell>
+                                <TableCell>
+                                    <IconButton onClick={ () => {deletar(imo.id)} }><DeleteIcon /> </IconButton>
+                                </TableCell>
                             </TableRow>
                         )
                         })
